29271c7af8b3804ee8b4a5f9de1f701746a55ba2
aminjonshermatov <aminjonshermatov@gmail.com>
2022-07-11 21:34:41 +0500

    neo4j files

--- backend/v1/v1.py
+++ backend/v1/v1.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 
 from backend.v1.util import neo4j_driver
 
@@ -31,6 +31,20 @@ async def get_node(node_uuid: str):
     return data
 
 
-@router.get('/file/${node_id}')
-async def load_file(node_id: str):
-    return f'file ${node_id}'
+@router.get('/file/{node_uuid}')
+async def load_file(node_uuid: str):
+    with neo4j_driver.session() as session:
+        try:
+            found_file = session.run('''match (f_node:FileNode {{ uuid: '{0}' }}) return f_node'''.format(node_uuid)).data()
+
+            if len(found_file) != 1:
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                    detail=f'File with id: {node_uuid} not found.')
+
+            found_file = found_file[0]
+        except HTTPException as http_ex:
+            raise http_ex
+        except Exception as ex:
+            print(f'Exception while streaming file: {ex}')
+
+    return f'file ${node_uuid}'

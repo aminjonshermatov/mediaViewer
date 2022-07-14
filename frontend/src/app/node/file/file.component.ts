ee410bd8237d52ba104d6f61e948745a27e12b0e
aminjonshermatov <aminjonshermatov@gmail.com>
2022-07-15 00:55:36 +0500

    frontend grid list done

---
+++ frontend/src/app/node/file/file.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {IFile} from "../models/File";
+
+@Component({
+  selector: 'app-file',
+  templateUrl: './file.component.html',
+  styleUrls: ['./file.component.scss']
+})
+export class FileComponent implements OnInit {
+
+  @Input() file!: IFile;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ frontend/src/app/node/folder/folder.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {IFolder} from "../models/Folder";
+
+@Component({
+  selector: 'app-folder',
+  templateUrl: './folder.component.html',
+  styleUrls: ['./folder.component.scss']
+})
+export class FolderComponent implements OnInit {
+
+  @Input() folder!: IFolder;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
